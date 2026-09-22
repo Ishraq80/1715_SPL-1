ba6c74ab015f1360e2c27bbd0b43a9d097bdd6b7
Ishraq Naim <ishraqnaim80@gmail.com>
2026-09-23 01:25:22 +0600

    Update imageinit.cpp

--- imageinit.cpp
+++ imageinit.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <openslide/openslide.h>
 
 using namespace std;
@@ -29,28 +28,6 @@ int main() {
     int32_t num_levels = openslide_get_level_count(slide);
     cout << "Number of levels: " << num_levels << endl;
 
-    int32_t target_level = 0; 
-    int64_t x_start = 0;
-    int64_t y_start = 0;
-    int64_t tile_w = 512;
-    int64_t tile_h = 512;
-
-    vector<uint32_t> pixel_buffer(tile_w * tile_h);
-
-    openslide_read_region(slide, pixel_buffer.data(), x_start, y_start, target_level, tile_w, tile_h);
-
-    const char* error_msg = openslide_get_error(slide);
-    if (error_msg != nullptr) 
-    {
-        cerr << "Error reading region: " << error_msg << endl;
-    }
-    else 
-    {
-        cout << "Successfully read region!" << endl;
-    }
     openslide_close(slide);
     return 0;
 }
-
-
-// g++ imageinit.cpp -IC:/openslide-bin-4.0.1.2-windows-x64/openslide-bin-4.0.1.2-windows-x64/include -LC:/openslide-bin-4.0.1.2-windows-x64/openslide-bin-4.0.1.2-windows-x64/lib -lopenslide -o test.exe
